0e51e6ca65c1664c695ae377d38017eab4b99fc6
Nicole Hall <nicolealexandrahall@gmail.com>
2015-09-15 14:06:10 -0600

    converted hash back to array upon second thought

--- lib/key.rb
+++ lib/key.rb
@@ -1,10 +1,10 @@
 class Key
-  attr_accessor :key, :date, :position_hash
+  attr_accessor :key, :date, :position_array
 
   def initialize
     @key = generate_random_key
     @date = Time.now.strftime("%d%m%y")
-    @position_hash = {}
+    @position_array = []
   end
 
   def generate_random_key
@@ -17,14 +17,16 @@ class Key
 
   def complete_key
     date_squared = (@date.to_i * @date.to_i).to_s
-    @position_hash[:a_rot] = ((@key[0] + @key[1]).to_i + (date_squared[-4]).to_i).to_s
-    @position_hash[:b_rot] = ((@key[1] + @key[2]).to_i + (date_squared[-3]).to_i).to_s
-    @position_hash[:c_rot] = ((@key[2] + @key[3]).to_i + (date_squared[-2]).to_i).to_s
-    @position_hash[:d_rot] = ((@key[3] + @key[4]).to_i + (date_squared[-1]).to_i).to_s
+    @position_array << ((@key[0] + @key[1]).to_i + (date_squared[-4]).to_i).to_s
+    @position_array << ((@key[1] + @key[2]).to_i + (date_squared[-3]).to_i).to_s
+    @position_array << ((@key[2] + @key[3]).to_i + (date_squared[-2]).to_i).to_s
+    @position_array << ((@key[3] + @key[4]).to_i + (date_squared[-1]).to_i).to_s
   end
 
 end
 
-# key1 = Key.new
-# key1.key = "12345"
-# key1.date = "140915"
+key1 = Key.new
+key1.key = "12345"
+key1.date = "140915"
+key1.complete_key
+p key1.position_array
