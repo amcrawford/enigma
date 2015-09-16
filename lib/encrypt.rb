8bb63a997b6794a8016ca349224d55f5c8d6d3d5
Amber Crawford <ammcrawford@gmail.com>
2015-09-15 18:15:51 -0600

    Key and Encrypt passing

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -2,29 +2,32 @@ require_relative 'key'
 
 class Encrypt
 CHARACTER_MAP = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", " ", ".", ","]
-  attr_reader :character_map, :start_key
-  #have a character map
+  attr_reader :start_key
 
   def initialize
-    @start_key = Key.complete_key
+    @start_key = Key.new
+  end
+
+  def complete_key(date = Time.now.strftime("%d%m%y"), random_key = generate_random_key)
+    @start_key.complete_key(date, random_key)
   end
 
   def split_plain_text(plain_text)
     plain_text.downcase.chars.each_slice(4).to_a
   end
 
-  def encrypt(plain_text)
+  def encrypt(plain_text, key = @start_key.complete_key)
     encrypted_text = ""
     chunks_of_four = split_plain_text(plain_text)
     chunks_of_four.each do |chunk|
-      encrypted_text = rotate(chunk).join
+      encrypted_text << rotate(chunk, key)
     end
     encrypted_text
   end
 
-  def rotate(chunk, key = @start_key)
+  def rotate(chunk, key = @start_key.complete_key)
     encrypted_message = ""
-      array.each_with_index do |char, index|
+      chunk.each_with_index do |char, index|
           current_map = CHARACTER_MAP
           loop do
             current_map = current_map.rotate
@@ -34,14 +37,9 @@ CHARACTER_MAP = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m"
           encrypted_character = current_map[0]
           encrypted_message << encrypted_character
         end
-      end
       encrypted_message
-      end
     end
     #and then at the same index in the key, rotate through the char map by the rotations at the same index
-  end
-
-
 end
 
 # secret_message = Encrypt.new("a")
--- lib/file_io.rb
+++ lib/file_io.rb
@@ -6,12 +6,15 @@ class FileIO
   def initialize(plain, encrypted)
     @plain_message = File.open(ARGV[0]).read
     @encrypted_message = File.open(ARGV[1], "w")
-    #
-    @final_text = ""
-    key = Key.new
+    key = Encrypt.new.complete_key
     puts "Created '#{ARGV[1]}' with the key #{key.key} and date #{key.date}"
   end
 
+  def write
+    final_text = Encrypt.new.encrypt(@plain_message)
+    @encrypted_message.write(final_text)
+  end
 end
 
 file = FileIO.new(ARGV[0], ARGV[1])
+file.write
